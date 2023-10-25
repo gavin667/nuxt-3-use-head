148ba54337e3c4e007e08c804113027eee94e561
Gavin Kemp <gavin@sungod.co>
2023-10-25 09:43:33 +0100

    Reproduce issue unhead + useFetch issue

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>This is the index.vue page</h1>
+</template>
+
+<script>
+export default {
+    async setup() {
+        const { data: productResponse } = await useFetch('https://dummyjson.com/products/1')
+
+        console.log('productResponse', productResponse)
+
+        useHead({
+            title: 'Hello there'
+        })
+
+        return { hello: 'there' }
+    }
+}
+</script>
